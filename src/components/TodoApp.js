129efe74b98208c301079aebc97ff4dba6a8ae95
Aditya Inapurapu <adityaii@gmail.com>
2020-08-06 17:55:00 -0700

    initial commit

---
+++ src/components/TodoApp.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class TodoApp extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from create react app</h1>
+                <p>I am in a React Component!</p>
+            </div>
+        );
+    }
+}
+export default TodoApp;
+
