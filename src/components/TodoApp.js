c5ebd26fe94ac8d0895b72fc9e9f794e1cae5089
aialerts@yahoo.com <aialerts@yahoo.com>
2020-08-08 22:38:05 +0000

    Created TodoApp initial and Todos.js

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,11 +1,33 @@
 import React from "react";
 
 class TodoApp extends React.Component {
+
+        state = {
+            todos: [
+                {
+                    id: 1,
+                    title: "Setup development environment",
+                    completed: true
+                },
+                {
+                    id: 2,
+                    title: "Develop website and add content",
+                    completed: false
+                },
+                {
+                    id: 1,
+                    title: "Deploy to live server",
+                    completed: false
+                }
+            ]
+        }
     render() {
+        // Console.log(this.state.todos)
         return (
             <div>
-                <h1>Hello from create react app</h1>
-                <p>I am in a React Component!</p>
+                {this.state.todos.map(todo => (
+                    <li>{todo.title}</li>
+                ))}
             </div>
         );
     }
---
+++ src/components/Todos.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+class Todos extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello from Todos
+            </div>
+        );
+    }
+}
+export default Todos
